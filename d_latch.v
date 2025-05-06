1306f25c4de820868475809e78be7ded6078838a
PawlosAD <156163244+PawlosAD@users.noreply.github.com>
2025-05-06 08:44:10 -0600

    Update d_latch.v

--- d_latch.v
+++ d_latch.v
@@ -1,16 +1,37 @@
 `timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/25/2025 02:25:42 PM
+// Design Name: 
+// Module Name: d_latch
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
 
 module d_latch(
-    input d,
-    input enable,
-    output reg q
-    );
+    input D,
+    input E,
+    output reg Q,
+    output NotQ
+);
 
-    always @ (enable, d)
-    begin
-        if (enable)
-            q = d;
+    always @(D, E) begin 
+        if (E)
+            Q <= D; 
     end
 
-endmodule
+    assign NotQ = ~Q; 
 
+endmodule
