14edff2f981966e8ff957c65bada6763644c2f99
PawlosAD <156163244+PawlosAD@users.noreply.github.com>
2025-05-06 08:42:44 -0600

    Update byte_memory.v

--- byte_memory.v
+++ byte_memory.v
@@ -1,16 +1,36 @@
 `timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/25/2025 02:26:29 PM
+// Design Name: 
+// Module Name: byte_memory
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
 
 module byte_memory(
     input [7:0] data,
-    input clk,
     input store,
     output reg [7:0] memory
 );
 
-    always @(posedge clk) begin
+    always @(store, data) begin 
         if (store)
-            memory <= data;
+            memory <= data; 
     end
 
 endmodule
+
 
