426f3ed5513863e2800112d6fc3df76b9600e088
PawlosAD <156163244+PawlosAD@users.noreply.github.com>
2025-05-08 00:42:55 -0600

    Update byte_memory.v

--- byte_memory.v
+++ byte_memory.v
@@ -4,7 +4,7 @@
 // Engineer: 
 // 
 // Create Date: 03/25/2025 09:40:44 AM
-// Design Name: 
+// Design Name:
 // Module Name: byte_memory
 // Project Name: 
 // Target Devices: 
@@ -21,13 +21,16 @@
 
 
 module byte_memory(
+    // This is a module that implements a single 8 bit memory register with a load enable
     input [7:0] data,
     input store,
     output reg [7:0] memory
 );
-
+    // The always block triggers whenever store or data changes and it makes sure that the memory register updates as soon as either the control signal or input data changes.
     always @(store, data) begin 
+        // checks if the store control signal is active
         if (store)
+            // if store control signal is active, then load the input data value into the 'memory'
             memory <= data; 
     end
 
