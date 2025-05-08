30ba2151d5a722e25fffc86bf60322451826d5b7
PawlosAD <156163244+PawlosAD@users.noreply.github.com>
2025-05-08 01:52:38 -0600

    Update top.v

--- top.v
+++ top.v
@@ -19,23 +19,23 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
+// Connects user inputs to memory sytem and latch to display outputs on LEDs
 module top(
-    input [15:0] sw,
-    input btnC,
-    output [15:0] led
+    input [15:0] sw, // 16 slide switches on board as input
+    input btnC, // Center pushbutton as control signal
+    output [15:0] led /// 16 LEDs on board as output
 );
     d_latch part1(
-        .D(sw[0]),
-        .Q(led[0]),
-        .NotQ(led[1]),
-        .E(btnC)
+        .D(sw[0]), // Data input from switch 0
+        .Q(led[0]), // Output Q to LED 0
+        .NotQ(led[1]), // Inverted output to LED 1
+        .E(btnC) // Enable latch with center button
     );
     memory_system part2(
-        .data(sw[15:8]),
-        .addr(sw[7:6]),
-        .store(btnC),
-        .memory(led[15:8])
+        .data(sw[15:8]), // 8 bit data input from switches 15-8
+        .addr(sw[7:6]), // 2 bit address input from switches 7-6
+        .store(btnC), // Store control from center button
+        .memory(led[15:8]) // 8 bit memory output to LEDs 15-8
     );
 
 endmodule
