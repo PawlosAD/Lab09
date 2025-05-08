a1d639b710c7b502a0b75db67a0b89b871563543
PawlosAD <156163244+PawlosAD@users.noreply.github.com>
2025-05-08 00:57:50 -0600

    Update demux1.v

--- demux1.v
+++ demux1.v
@@ -19,7 +19,7 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
+// Implements 1 bit 1 to 4 demultiplexer with a 2 bit selection line
 module demux1(
     input data,
     input [1:0] sel,
@@ -30,11 +30,17 @@ module demux1(
 
 );
 
+    // Always block triggers whenever data or sel changes.
     always @(*) begin 
+        // case statement uses sel to figure with output line would receive data while others are reset to 0.
         case(sel)
+            // Send data to output A, and others get 0 if sel is 2'b00
             2'b00: {D, C, B, A} <= {1'b0, 1'b0, 1'b0, data};
+            // Send data to output B, and others get 0 if sel is 2'b01
             2'b01: {D, C, B, A} <= {1'b0, 1'b0, data, 1'b0};
+            // Send data to output C, and others get 0 if sel is 2'b10
             2'b10: {D, C, B, A} <= {1'b0, data, 1'b0, 1'b0};
+            // Send data to output D, and others get 0 if sel is 2'b11
             2'b11: {D, C, B, A} <= {data, 1'b0, 1'b0, 1'b0};
         endcase
     end
